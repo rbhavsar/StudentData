74cea4f1996fcfac85205442ee03df9f4418d888
Ravi Bhavsar <rbhavsar@birst.com>
2017-10-22 16:49:49 -0700

    Read data from Properties file

--- src/com/emailapp/EmailApp.java
+++ src/com/emailapp/EmailApp.java
@@ -5,6 +5,12 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class EmailApp {
 
     private final static Logger log = Logger.getLogger(EmailApp.class.getName());
@@ -20,15 +26,31 @@ public class EmailApp {
         log.fatal("This is Fatal");
 
 
-        Email email = new Email("Ravi","Bhavsar");
+        File file = new File("/Users/rbhavsar/StudentData/Data/src/com/emailapp/application.properties");
+        Properties prop = new Properties();
+        FileInputStream fileInput = null;
 
         try {
-            email.setAlternateEmail("rbhavsar@gmail.com");
+            log.info("fileInput Stream "+file);
+            fileInput = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            log.error("File not found "+e.getMessage());
+            e.printStackTrace();
         }
-        catch(Exception e)
-        {
-            log.error("Error while Set Alternate Email "+e.getMessage());
+
+        //load properties file
+        try {
+            log.info("Load properties file "+fileInput);
+            prop.load(fileInput);
+        } catch (IOException e) {
+            log.error("Error while load Properties "+e.getMessage());
+            e.printStackTrace();
         }
+
+        Email email = new Email(prop.getProperty("firstName"),prop.getProperty("lastName"));
+        email.setAlternateEmail("rbhavsar@gmail.com");
+
+
         //System.out.println("alternateEmail :"+email.getAlternateEmail());
 
         email.setMailBoxCapacity(100);
