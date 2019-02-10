37151ca0615b530f77c2ee52ea0de0e575c0e003
Ravi Bhavsar <rbhavsar@birst.com>
2019-02-10 12:44:52 -0800

    initial commit

--- src/com/CodingBat/String1Copy.java
+++ src/com/CodingBat/String1Copy.java
@@ -6,7 +6,7 @@ public class String1 {
 
         //Check stash 123
         //Test
-        //com.com
+        //com.com , com.com
        String value = everyNth("RaviBhavsar",2);
        //System.out.println("Value :-"+value);
 
