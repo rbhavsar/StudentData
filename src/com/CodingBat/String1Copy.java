1972fe53ece15cc44b5f8b01e5e62181feda95d5
Ravi Bhavsar <rbhavsar@birst.com>
2019-02-10 21:00:52 -0800

    Pizza

--- src/com/CodingBat/String1Copy.java
+++ src/com/CodingBat/String1Copy.java
@@ -7,6 +7,7 @@ public class String1 {
         //Check stash 123
         //Test
         //For testing purpose
+        //Pizza
        String value = everyNth("RaviBhavsar",2);
        //System.out.println("Value :-"+value);
 
