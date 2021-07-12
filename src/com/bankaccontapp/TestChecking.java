9e3c0197c6400b1293aa8b81b70322216f159f7f
Ravi Bhavsar <Ravi.Bhavsar@infor.com>
2021-07-11 20:17:07 -0700

    cal-div first

--- src/com/bankaccontapp/TestChecking.java
+++ src/com/bankaccontapp/TestChecking.java
@@ -7,7 +7,8 @@ public class Checking extends Account {
     private int debitCardPIN;
     private int number;
     private int cal_test;
-    private int cal_test2;
+    private int cal_test3;
+    private int cal_div;
 
     //Constructor to initialize checking acconut properties
     public Checking(String name,String ssn,double initDeposit){
